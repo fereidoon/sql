30d6c36e54e92fd602b6aa26197b2a5292ee7e03
fereidoon <ferydoon@gmail.com>
2018-12-25 11:42:34 +0330

    last

--- 017_sql.py
+++ 017_sql.py
@@ -1,10 +1,12 @@
 import sqlite3
 with sqlite3.connect("cars.db") as conn:
 	c=conn.cursor()
-	c.execute("""SELECT inventory.make,inventory.model,inventory.quantity 
+	c.execute("""SELECT inventory.make,inventory.model,inventory.quantity, 
 	orders.order_date FROM inventory INNER JOIN orders
 	ON 	inventory.model = orders.model""")
-	#c.execute("""SELECT inventory.make, inventory.model,
-     #       inventory.quantity, orders.order_date FROM inventory INNER JOIN orders
-      #      ON inventory.model = orders.model""")
-
+	row=c.fetchall()
+	for r in row:
+		print(r[0],r[1])
+		print(r[2])
+		print(r[3])
+		print()
