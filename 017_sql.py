3fb539ecb819548e66391bf7f1022c8bb735b09f
userf k <userf@userftest.localdomain>
2018-12-25 08:36:30 +0000

    sgfdfg

--- 017_sql.py
+++ 017_sql.py
@@ -1,7 +1,7 @@
 import sqlite3
 with sqlite3.connect("cars.db") as conn:
 	c=conn.cursor()
-	c.execute("""SELECT inventory.make,inventory.model,inventory.quantity 
+	c.execute("""SELECT inventory.make,inventory.model,inventory.quantity,
 	orders.order_date FROM inventory INNER JOIN orders
 	ON 	inventory.model = orders.model""")
 	#c.execute("""SELECT inventory.make, inventory.model,
